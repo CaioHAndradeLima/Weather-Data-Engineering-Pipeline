953cb871d1196b053c4db2bcb2fc578a142cb2cc
Caio Henrique <caiohenrique@Caios-MacBook-Air-2.local>
2026-01-02 15:55:17 -0300

    dag python to ingest weather info

---
+++ airflow/dags/ingest_noaa_annual_dag.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+
+# Import the ingestion logic
+import sys
+sys.path.append("/opt/airflow")
+from scripts.ingest_noaa_annual import run
+
+default_args = {
+    "owner": "data-engineering",
+    "depends_on_past": False,
+    "retries": 2,
+    "retry_delay": timedelta(minutes=10),
+}
+
+
+with DAG(
+    dag_id="ingest_noaa_annual",
+    description="Annual incremental ingestion of NOAA weather data into Bronze layer",
+    default_args=default_args,
+    start_date=datetime(2024, 1, 1),
+    schedule_interval="@yearly",
+    catchup=False,
+    tags=["noaa", "weather", "bronze", "ingestion"],
+) as dag:
+    start = EmptyOperator(task_id="start")
+    end = EmptyOperator(task_id="end")
+
+    ingest_noaa_annual = PythonOperator(
+        task_id="ingest_noaa_annual",
+        python_callable=run,
+    )
+
+    start >> ingest_noaa_annual >> end
